3361f7408169845430ddb28f466ee69e3f6aeb47
mrcbq <marcobaquero@gmail.com>
2024-02-05 05:03:59 -0500

    Add gh-pages to deploy, delete unnecesary comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchSlice from './slices/searchSlice';
-import animeSlice from './slices/animeSlice';
 
 const store = configureStore({
   reducer: {
     search: searchSlice,
-    anime: animeSlice,
   },
 });
 
--- src/utils/calculateAverageScore.js
+++
@@ -1,4 +0,0 @@
-const calculateAverageScore = (seasons) => {
-};
-
-export default calculateAverageScore;
