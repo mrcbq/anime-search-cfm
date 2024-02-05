0a8b7e92954fe54f40c67107dccfd6181b38530d
mrcbq <marcobaquero@gmail.com>
2024-02-04 20:15:42 -0500

    Add redux, Add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="AnimeApp">
+      <h1>Anime Search</h1>
+    </div>
   )
 }
 
